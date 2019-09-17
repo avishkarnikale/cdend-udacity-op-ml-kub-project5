245c7051a759d6b3ce094085d0291836731158ad
Avishkar Nikale <neo.avishkar@gmail.com>
2019-09-17 12:03:37 +0000

    Task2 - Create docker image & run and predict 1 value

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,11 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-
+sudo docker build --tag=cdend-uda-avish-proj5 .
 # Step 2: 
 # List docker images
-
-# Step 3: 
+sudo docker image ls
 # Run flask app
+sudo docker run -p 8080:80 cdend-uda-avish-proj5
+
+
