a660e1fa298db872d27b88dc128c4ebce16f3ce0
Avishkar Nikale <neo.avishkar@gmail.com>
2019-09-17 16:17:30 +0000

    Task5 - Upload image to docker

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="avishkarn/cdend-uda-avish-proj5:latest"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+sudo docker login --username=avishkarn
+sudo docker tag cdend-uda-avish-proj5 $dockerpath
 # Step 3:
 # Push image to a docker repository
+sudo docker push $dockerpath
+
