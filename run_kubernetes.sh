5bd3cdf38964a4d21efcdcc785aa037650c34cbd
Avishkar Nikale <neo.avishkar@gmail.com>
2019-09-17 17:04:08 +0000

    Task6 7 - Run and deploy on k8s cluster and get prediction output from pod

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="cdend-uda-avish-proj5"
+
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+sudo kubectl run $dockerpath --image=avishkarn/cdend-uda-avish-proj5:latest --port=80
 
 # Step 3:
 # List kubernetes pods
+sudo kubectl get pod
 
 # Step 4:
 # Forward the container port to a host
+sudo kubectl expose deployment cdend-uda-avish-proj5 --type=LoadBalancer --port 80
+minikube service cdend-uda-avish-proj5
 
